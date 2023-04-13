92f040d907dacb8d00224b6001f74586438f9767
nayebuljannath780 <chynayeb780@gmail.com>
2023-04-13 15:29:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
+app.get("/", (req, res) => {
+  res.send("News API Running");
+});
+
+app.get("/category", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.listen(port, () => {
+  console.log("Eminent-express is running on port", port);
+});
